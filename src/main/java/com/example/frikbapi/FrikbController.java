68883bc82a23c366df7e390d12f8bb7d68e26976
Frenske99 <39583221+Frenske99@users.noreply.github.com>
2022-04-28 11:08:08 +0200

    first version still needs work

---
+++ src/main/java/com/example/frikbapi/FrikbController.java
@@ -0,0 +1,17 @@
+package com.example.frikbapi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FrikbController {
+
+    @GetMapping("/hello-world")
+    @ResponseBody
+    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+        return new Greeting("leon is een van de beste zoniet de beste leraar");
+    }
+
+}
---
+++ src/main/java/com/example/frikbapi/Greeting.java
@@ -0,0 +1,19 @@
+package com.example.frikbapi;
+
+public class Greeting {
+    private String s;
+
+    public Greeting(String s) {
+
+        this.s = s;
+    }
+
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+}
